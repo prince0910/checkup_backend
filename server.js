befdcf6ed0aa06cebb245a6930b51edb4ccf2359
prince <111260320+prince0910@users.noreply.github.com>
2022-08-31 15:29:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.use(session({
 
  
 app.use(flash());
- 
  /////////test
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
